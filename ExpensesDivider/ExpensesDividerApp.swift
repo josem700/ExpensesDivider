588d9d5b900f6a62e5bfd09f1a01552676067c5a
josem700 <jgarciajosemanuel@gmail.com>
2024-07-04 17:44:43 +0200

    Initial Commit

---
+++ ExpensesDivider/ExpensesDividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesDividerApp.swift
+//  ExpensesDivider
+//
+//  Created by Jose M on 4/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
